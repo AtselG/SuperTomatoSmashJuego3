82463cccd6aa8dc1c7a58c68b65442f6a140e275
elatsel <80478653+elatsel@users.noreply.github.com>
2021-10-12 19:50:48 -0300

    Agregar archivos de proyecto.

---
+++ SuperTomatoSmashJuego3/FormInicio.vb
@@ -0,0 +1,24 @@
+﻿Public Class FormInicio
+    Private Sub ButtonJugar_Click(sender As Object, e As EventArgs) Handles ButtonJugar.Click
+        If SoundOnOff = True Then
+            My.Computer.Audio.Play(My.Resources.Drip_2, AudioPlayMode.Background)
+        End If
+        FormDificultades.ShowDialog(owner:=Me)
+    End Sub
+    Private Sub ButtonOpciones_Click(sender As Object, e As EventArgs) Handles ButtonOpciones.Click
+        If SoundOnOff = True Then
+            My.Computer.Audio.Play(My.Resources.Drip_2, AudioPlayMode.Background)
+        End If
+        FormOpciones.Show()
+        Hide()
+    End Sub
+    Private Sub ButtonSalir_Click(sender As Object, e As EventArgs) Handles ButtonSalir.Click
+        If SoundOnOff = True Then
+            My.Computer.Audio.Play(My.Resources.Drip_2, AudioPlayMode.Background)
+        End If
+        Application.Exit()
+    End Sub
+    Private Sub FormInicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SoundOnOff = True
+    End Sub
+End Class
